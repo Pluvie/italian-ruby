da0767e1c41f2ddd303227fa0b96b1e1a7e2152b
Pluvie <francesco.ballardin@gmail.com>
2020-03-31 09:41:55 +0200

    aggiunti alias simboli

--- lib/italian/ruby/core_ext.rb
+++ lib/italian/ruby/core_ext.rb
@@ -13,6 +13,7 @@ require_relative "core_ext/object"
 require_relative "core_ext/array"
 require_relative "core_ext/hash"
 require_relative "core_ext/string"
+require_relative "core_ext/symbol"
 require_relative "core_ext/integer"
 require_relative "core_ext/enumerator"
 
---
+++ lib/italian/ruby/core_ext/symbol.rb
@@ -0,0 +1,10 @@
+##
+# Core Ext - Symbol
+#
+# Alias di metodi della classe Symbol.
+
+class Symbol
+  alias       :in_simbolo     :to_sym
+end
+
+Simbolo = Symbol
