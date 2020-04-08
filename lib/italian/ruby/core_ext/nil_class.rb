e6fcdf255cf9c7d8d2118e0687374d6055f1c333
Pluvie <francesco.ballardin@gmail.com>
2020-04-08 08:09:31 +0200

    aggiunto alias

--- lib/italian/ruby/core_ext/nil_class.rb
+++ lib/italian/ruby/core_ext/nil_class.rb
@@ -4,6 +4,7 @@
 class NilClass
   alias               :nullo?               :nil?
   alias               :nulla?               :nil?
+  alias               :in_stringa           :to_s
 
   def esiste?
     !nil?
