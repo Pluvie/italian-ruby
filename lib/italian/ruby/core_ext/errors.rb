e1fb0c3616af67e00211405fa59578fcf02e8544
Pluvie <francesco.ballardin@gmail.com>
2020-04-17 08:29:59 +0200

    aggiunto alias

---
+++ lib/italian/ruby/core_ext/errors.rb
@@ -0,0 +1,11 @@
+##
+# Core Ext - StandardError
+#
+# Alias di metodi della classe StandardError.
+
+class StandardError
+  alias     :messaggio        :message
+  alias     :traccia          :backtrace
+end
+
+Errore = StandardError
