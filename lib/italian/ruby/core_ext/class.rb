3c33da89412b7e514921df5eadf299cd0e33f2b7
Pluvie <francesco.ballardin@gmail.com>
2020-04-21 09:49:28 +0200

    aggiunto alias

--- lib/italian/ruby/core_ext/class.rb
+++ lib/italian/ruby/core_ext/class.rb
@@ -5,10 +5,8 @@ class Class
   alias       :privati                :private
   alias       :protetti               :protected
   alias       :pubblici               :public
-
   alias       :accessore              :attr_accessor
-
   alias       :definisci_metodo       :define_method
-
   alias       :apri_classe            :class_eval
+  alias       :moduli_inclusi         :included_modules
 end
