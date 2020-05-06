df119be7647524740a72e73efa757575131bb2ec
Pluvie <francesco.ballardin@gmail.com>
2020-05-06 15:47:08 +0200

    aggiunti alias

--- lib/italian/ruby/core_ext/array.rb
+++ lib/italian/ruby/core_ext/array.rb
@@ -52,9 +52,8 @@ class Array
   alias         :cerniera             :zip
   alias         :ordina               :sort
   alias         :ordina_per           :sort_by
-  alias         :ordina_per!          :sort_by
+  alias         :ordina_per!          :sort_by!
   alias         :raggruppa_per        :group_by
-  alias         :raggruppa_per!       :group_by
   alias         :indice               :index
   alias         :indice_da_destra     :rindex
   alias         :indice_dal_fondo     :rindex
