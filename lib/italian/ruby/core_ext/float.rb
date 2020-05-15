c0a957bc171868604e1d2361ed51d130047b094c
Pluvie <francesco.ballardin@gmail.com>
2020-05-15 09:12:48 +0200

    aggiunto alias

--- lib/italian/ruby/core_ext/float.rb
+++ lib/italian/ruby/core_ext/float.rb
@@ -4,6 +4,8 @@
 # Alias di metodi della classe Float.
 
 class Float
+  INFINITO = INFINITY
+
   alias       :in_intero                    :to_i
   alias       :arrotonda                    :round
   alias       :arrotonda_per_eccesso        :ceil
