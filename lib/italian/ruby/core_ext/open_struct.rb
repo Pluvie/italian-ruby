919e3be474f7dd3cc5085cf23c8d3a49618a9be2
Pluvie <francesco.ballardin@gmail.com>
2020-06-22 11:21:53 +0200

    migliorata conversione in mappa di archivio

--- lib/italian/ruby/core_ext/open_struct.rb
+++ lib/italian/ruby/core_ext/open_struct.rb
@@ -1,10 +1,21 @@
+require "ostruct"
+
 ##
 # Core Ext - OpenStruct
 #
 # Alias di metodi della classe OpenStruct.
 
 class OpenStruct
-  alias         :in_mappa                   :to_h
+  def in_mappa(*args)
+    hash = to_h *args
+    hash.in_mappa do |key, value|
+      if value.respond_to? :to_h
+        [ key, value.to_h ]
+      else
+        [ key, value ]
+      end
+    end
+  end
 end
 
 Archivio = OpenStruct
