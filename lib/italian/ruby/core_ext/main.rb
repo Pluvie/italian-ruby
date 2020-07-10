3e97bef7bde6f9fc32b056979f177e8db89a2510
Pluvie <francesco.ballardin@gmail.com>
2020-07-10 11:58:33 +0200

    aggiunto alias

--- lib/italian/ruby/core_ext/main.rb
+++ lib/italian/ruby/core_ext/main.rb
@@ -6,6 +6,7 @@
 alias     :stampa                       :puts
 alias     :scrivi                       :print
 alias     :cicla                        :loop
+alias     :dormi                        :sleep
 alias     :definisci_metodo_singolo     :define_singleton_method
 alias     :blocco_dato?                 :block_given?
 alias     :cattura                      :catch
