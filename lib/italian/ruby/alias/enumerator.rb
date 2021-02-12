5a41eb8a18176019925a14ad499a98a9de36f504
Pluvie <francesco.ballardin@gmail.com>
2021-02-12 09:50:08 +0100

    aggiunti alias

--- lib/italian/ruby/alias/enumerator.rb
+++ lib/italian/ruby/alias/enumerator.rb
@@ -3,6 +3,11 @@
 class Enumerator
 
   alias       :mappa          :map
+  alias       :per_ognuno     :each
+  alias       :per_ognuna     :each
+  alias       :lista          :to_a
+  alias       :in_lista       :to_a
+  alias       :in_liste       :to_a
   alias       :con_indice     :with_index
 
 end
