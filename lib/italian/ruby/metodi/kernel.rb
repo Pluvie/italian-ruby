b74e9fab4664c4163c2ac7bb2c300b19d3c0c0e0
Pluvie <francesco.ballardin@gmail.com>
2021-02-24 06:35:35 +0100

    aggiunto rimanda e rimanda_relativo

--- lib/italian/ruby/metodi/kernel.rb
+++ lib/italian/ruby/metodi/kernel.rb
@@ -38,12 +38,8 @@ module Kernel
   ##
   # Richiede un archivio cercandolo come percorso relativo.
   def richiedi_relativo(percorso_archivio)
-    sorgente_chiamante      = Italian::Ruby.percorso_archivio_non_tradotto caller_locations.first.absolute_path
-    linea_chiamante         = caller_locations.first.lineno
-    cartella_chiamante      = File.dirname sorgente_chiamante
-    archivio_da_richiedere  = File.expand_path File.join(cartella_chiamante, percorso_archivio)
-    archivio_da_richiedere  = Dir["#{archivio_da_richiedere}.{ir,rb}"].compact.first
-
+    sorgente_chiamante, linea_chiamante = ottieni_sorgente_e_linea_chiamante caller_locations
+    archivio_da_richiedere = archivio_relativo sorgente_chiamante, percorso_archivio
     raise LoadError, "cannot load such file -- #{percorso_archivio}" if archivio_da_richiedere.nil?
     traduci_e_richiedi archivio_da_richiedere
   rescue LoadError => errore
@@ -53,12 +49,8 @@ module Kernel
   ##
   # Richiede tutti gli archivi di una cartella.
   def richiedi_tutti(percorso_cartella)
-    sorgente_chiamante      = Italian::Ruby.percorso_archivio_non_tradotto caller_locations.first.absolute_path
-    linea_chiamante         = caller_locations.first.lineno
-    cartella_chiamante      = File.dirname sorgente_chiamante
-    cartella_da_richiedere  = File.expand_path File.join(cartella_chiamante, percorso_cartella)
-    cartella_da_richiedere  = nil unless Dir.exist? cartella_da_richiedere
-
+    sorgente_chiamante, linea_chiamante = ottieni_sorgente_e_linea_chiamante caller_locations
+    cartella_da_richiedere = cartella_relativa sorgente_chiamante, percorso_cartella
     raise LoadError, "cannot load such directory -- #{percorso_cartella}" if cartella_da_richiedere.nil?
     Dir["#{cartella_da_richiedere}/**/*.ir"].sort_by { |file| file.count("/") }.each do |archivio|
       traduci_e_richiedi archivio
@@ -67,20 +59,73 @@ module Kernel
     Italian::Ruby::Errori.recupera errore, sorgente_chiamante, linea_chiamante
   end
 
+  ##
+  # Rimanda la costante alla definizione in archivio. La prima volta che viene
+  # incontrata la definizione della costante nel codice, allora l'archivio
+  # verrà richiesto.
+  def rimanda(costante, archivio)
+    archivi_in_memoria = $:.map do |cartella|
+      Dir["#{cartella}/**/#{archivio}.ir"]
+    end
+    archivio_in_italiano_da_richiedere = archivi_in_memoria.flatten.compact.first
+    archivio_da_richiedere = Italian::Ruby.percorso_archivio_tradotto archivio_in_italiano_da_richiedere
+    Object.autoload costante, archivio_da_richiedere
+  end
+
+  ##
+  # Come il rimanda, ma usando un percorso relativo.
+  def rimanda_relativo(costante, percorso_archivio)
+    sorgente_chiamante, linea_chiamante = ottieni_sorgente_e_linea_chiamante caller_locations
+    archivio_da_richiedere = archivio_relativo sorgente_chiamante, percorso_archivio
+    archivio_da_richiedere = Italian::Ruby.percorso_archivio_tradotto archivio_da_richiedere
+    Object.autoload costante, archivio_da_richiedere
+  end
+
   private
+
+    ##
+    # Traduce un file da ItalianRuby a Ruby.
+    def traduci(archivio)
+      Italian::Ruby::Traduttore.traduci archivio
+    end
 
     ##
     # Traduce un file da ItalianRuby a Ruby e lo carica in memoria.
     def traduci_e_carica(archivio)
-      archivio_tradotto = Italian::Ruby::Traduttore.traduci archivio
-      load archivio_tradotto
+      load traduci(archivio)
     end
 
     ##
     # Traduce un file da ItalianRuby a Ruby e lo richiede.
     def traduci_e_richiedi(archivio)
-      archivio_tradotto = Italian::Ruby::Traduttore.traduci archivio
-      require archivio_tradotto
+      require traduci(archivio)
+    end
+
+    ##
+    # Ottiene sorgente e linea chiamante.
+    def ottieni_sorgente_e_linea_chiamante(traccia_chiamata)
+      sorgente_chiamante      = Italian::Ruby.percorso_archivio_non_tradotto traccia_chiamata.first.absolute_path
+      linea_chiamante         = traccia_chiamata.first.lineno
+
+      [ sorgente_chiamante, linea_chiamante ]
+    end
+
+    ##
+    # Ottiene il percorso archivio relativo del sorgente chiamante.
+    def archivio_relativo(sorgente_chiamante, percorso_archivio)
+      cartella_chiamante      = File.dirname sorgente_chiamante
+      archivio_da_richiedere  = File.expand_path File.join(cartella_chiamante, percorso_archivio)
+      archivio_da_richiedere  = Dir["#{archivio_da_richiedere}.{ir,rb}"].compact.first
+      archivio_da_richiedere
+    end
+
+    ##
+    # Ottiene il percorso cartella relativo del sorgente chiamante.
+    def cartella_relativa(sorgente_chiamante, percorso_cartella)
+      cartella_chiamante      = File.dirname sorgente_chiamante
+      cartella_da_richiedere  = File.expand_path File.join(cartella_chiamante, percorso_cartella)
+      cartella_da_richiedere  = nil unless Dir.exist? cartella_da_richiedere
+      cartella_da_richiedere
     end
 
 end
