aa407bd37fdd431a35bd398484fe8549f66aece3
Pluvie <francesco.ballardin@gmail.com>
2021-05-26 11:27:21 +0200

    aggiunti alias

--- lib/italian/ruby/alias/nil_class.rb
+++ lib/italian/ruby/alias/nil_class.rb
@@ -5,6 +5,8 @@ class NilClass
   alias               :nullo?               :nil?
   alias               :nulla?               :nil?
   alias               :in_stringa           :to_s
+  alias               :in_intero            :to_i
+  alias               :in_decimale          :to_f
 
   alias               :non_è_nullo?         :esiste?
   alias               :non_è_nulla?         :esiste?
